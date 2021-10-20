5bc84984617465d8c7477dcf1eae3683cb996639
kangarooCV <57002272+kangarooCV@users.noreply.github.com>
2021-10-20 16:57:45 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/SemiGlobalMatching_master.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SemiGlobalMatching_master.dir/main.cpp.obj"
+  "CMakeFiles/SemiGlobalMatching_master.dir/src/SemiGlobalMatching.cpp.obj"
+  "CMakeFiles/SemiGlobalMatching_master.dir/src/sgm_util.cpp.obj"
+  "CMakeFiles/SemiGlobalMatching_master.dir/src/stdafx.cpp.obj"
+  "SemiGlobalMatching_master.exe"
+  "SemiGlobalMatching_master.exe.manifest"
+  "SemiGlobalMatching_master.pdb"
+  "libSemiGlobalMatching_master.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SemiGlobalMatching_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
